11b15f7004088e612627166ad7a3cc18a4360059
Nelly <gichohinelly386@gmail.com>
2024-05-14 17:30:40 +0300

    The finished Code

--- capital.js
+++ capital.js
@@ -2,10 +2,22 @@
 // Write a program that accepts a string as input, capitalizes the first letter of each word in the 
 // string, and then returns the result string.
 
+function words(a){
+    b=[]
+    a = a.split(" ")
+    for (let word of a){
+        word=word.split("")
+        word[0]= word[0].toUpperCase()
+        // console.log(word)
+        word=word.join("")
+        b.push(word)
+    }
+    b=b.join(" ")
+    console.log(b)
+}
 
-let text= prompt("Input a sentence: ")
-text.toUpperCase
+a="this is a line of text"
+words(a)
 
-console.log(text)
 
 
